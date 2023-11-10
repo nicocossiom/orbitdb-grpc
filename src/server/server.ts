68df9e9e1e29aff9becc42df09ab5f37c3e045b0
Nicolás Cossío Miravalles <nicocossiom@gmail.com>
2023-11-10 16:08:37 +0100

    server mixup

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,7 @@
 import { connectNodeAdapter } from "@connectrpc/connect-node"
 import routes from "@server/connect"
 import { logger } from "@server/logger"
-import * as http2 from "http2"
+import http2 from "http2"
 
 const serverLogger = logger.child({ name: "server" })
 
@@ -14,18 +14,24 @@ server.on("sessionError", (err) => {
 })
 
 server.on("session", (session) => {
-  const sessionId = "client-" + session.socket.remoteAddress + ":" + session.socket.remotePort
-  const sessionLogger = logger.child({ name:  sessionId })
+  const sessionId = "client-" + session.socket.remoteAddress + ":" +
+    session.socket.remotePort
+  const sessionLogger = logger.child({ name: sessionId })
   sessionLogger.info("Session established with client")
   session.on("close", () => {
     sessionLogger.info("Session closed")
   })
 })
-
 // server.on("stream", (stream, headers) => {
 //   stream.
 // })
+//
 
-server.on("listening", () => serverLogger.info(`Server listening on http://${process.env.HOST}:${process.env.PORT || 4343}`))
+server.on("listening", () =>
+  serverLogger.info(
+    `Server listening on http://${process.env.HOST}:${
+      process.env.PORT || 4343
+    }`,
+  ))
 
 server.listen(parseInt(process.env.PORT) || 4343, process.env.HOST)
